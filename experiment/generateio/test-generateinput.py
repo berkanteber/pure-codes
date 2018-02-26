516b2f2325fbc42aeefb84c6443b1249aca41a57
Berkan Teber <berkanteber@sabanciuniv.edu>
2018-02-26 14:23:50 +0300

    structural change

--- experiment/generateio/test-generateinput.py
+++ experiment/generateio/test-generateinput.py
@@ -84,7 +84,7 @@ for m_input, w_input in itertools.product(inputsizes, inputsizes):
             resultfilename = inputfilename[:-8] + "result.txt"
 
             start = time.time()
-            command = "../clingo/clingo --stats {} codes/smpti.lp 1".format(inputfilename)
+            command = "../../clingo/clingo --stats {} ../codes/smpti.lp 1".format(inputfilename)
             process = subprocess.run(command, shell = True, stdout = subprocess.PIPE)
             output = process.stdout.decode('utf-8')
             end = time.time()
@@ -98,7 +98,7 @@ for m_input, w_input in itertools.product(inputsizes, inputsizes):
             resultfilename = inputfilename[:-8] + "result-sexequal.txt"
 
             start = time.time()
-            command = "../clingo/clingo --stats {} codes/smpti.lp codes/sexequal.lp 0".format(inputfilename)
+            command = "../../clingo/clingo --stats {} ../codes/smpti.lp ../codes/sexequal.lp 0".format(inputfilename)
             process = subprocess.run(command, shell = True, stdout = subprocess.PIPE)
             output = process.stdout.decode('utf-8')
             end = time.time()
@@ -112,7 +112,7 @@ for m_input, w_input in itertools.product(inputsizes, inputsizes):
             resultfilename = inputfilename[:-8] + "result-egalitarian.txt"
 
             start = time.time()
-            command = "../clingo/clingo --stats {} codes/smpti.lp codes/egalitarian.lp 0".format(inputfilename)
+            command = "../../clingo/clingo --stats {} ../codes/smpti.lp ../codes/egalitarian.lp 0".format(inputfilename)
             process = subprocess.run(command, shell = True, stdout = subprocess.PIPE)
             output = process.stdout.decode('utf-8')
             end = time.time()
@@ -126,7 +126,7 @@ for m_input, w_input in itertools.product(inputsizes, inputsizes):
             resultfilename = inputfilename[:-8] + "result-minregret.txt"
 
             start = time.time()
-            command = "../clingo/clingo --stats {} codes/smpti.lp codes/minregret.lp 0".format(inputfilename)
+            command = "../../clingo/clingo --stats {} ../codes/smpti.lp ../codes/minregret.lp 0".format(inputfilename)
             process = subprocess.run(command, shell = True, stdout = subprocess.PIPE)
             output = process.stdout.decode('utf-8')
             end = time.time()
@@ -140,7 +140,7 @@ for m_input, w_input in itertools.product(inputsizes, inputsizes):
             resultfilename = inputfilename[:-8] + "result-maxcardinality.txt"
 
             start = time.time()
-            command = "../clingo/clingo --stats {} codes/smpti.lp codes/maxcardinality.lp 0".format(inputfilename)
+            command = "../../clingo/clingo --stats {} ../codes/smpti.lp ../codes/maxcardinality.lp 0".format(inputfilename)
             process = subprocess.run(command, shell = True, stdout = subprocess.PIPE)
             output = process.stdout.decode('utf-8')
             end = time.time()
